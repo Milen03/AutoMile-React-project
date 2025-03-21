7ba2e6df2f236f8e72f061fbe87bcf214c32327b
Milen03 <milen.atanasovv03@gmail.com>
2025-03-21 19:18:14 +0200

    add auth api for reqst

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,3 +1,55 @@
+import { useContext, useEffect } from "react"
+import request from "../utils/request.js"
+import { UserContext } from "../context/userContext.js"
 
-const baseUrl = `http://localhost:3030/users`
+const baseUrl = 'http://localhost:3030/users'
+
+export const useLogin = () =>{
+
+    const login = async (email,password) =>{
+    const result =  await request.post(`${baseUrl}/login`,{email,password})
+
+    return result
+    }
+
+    return{
+        login,
+    }   
+}
+export const useRegister = () =>{
+
+    const register = async(email,password)=>{
+        const result = await request.post(`${baseUrl}/register`,{email,password})
+
+        return result
+    }
+
+    return{
+        register
+    }
+}
+
+export const useLogout = () =>{
+    const {accessToken , userLogoutHandeler} = useContext(UserContext)
+
+
+    useEffect(() =>{
+        if(!accessToken){
+            return
+        }
+        const option = {
+            headers: {
+                'X-Authorization': accessToken
+            }
+        };
+
+     request.get(`${baseUrl}/logout`,null,option)
+     .then(userLogoutHandeler({}));
+    
+    },[accessToken,userLogoutHandeler])
+     
+return {
+    isLoggedOut: !!accessToken,
+}
+}
 
